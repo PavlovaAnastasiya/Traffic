1ed2a42c4d451cc3678fe200d1fb57f8d1c73997
Pavlova <approdv@gmail.com>
2021-01-26 18:01:11 +0300

    Added press button

--- Traffic/ViewController.swift
+++ Traffic/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var lightColor: Int = 0
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        }
+    
+    fileprivate func updateUI() {
+        switch lightColor {
+        case 0:
+            view.backgroundColor = .red
+        case 1:
+            view.backgroundColor = .yellow
+        case 2:
+            view.backgroundColor = .green
+        default:
+            view.backgroundColor = .red
+            lightColor = 0
+        }
     }
-
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: Any, forEvent event: UIEvent) {
+        
+        lightColor += 1
+        updateUI()
+        
+    }
+    
 
 }
 
